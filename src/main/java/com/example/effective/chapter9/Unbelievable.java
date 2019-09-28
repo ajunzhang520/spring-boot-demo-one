d3df5610c0621b64d21700332d18b13e6d32ecb6
ajunzhang <1187977620@qq.com>
2019-09-28 16:47:20 +0800

    User: ajunzhang Description: Unbelievable

---
+++ src/main/java/com/example/effective/chapter9/Unbelievable.java
@@ -0,0 +1,16 @@
+package com.example.effective.chapter9;
+
+/**
+ * zhangshijun
+ * 2019/9/28 16:46
+ **/
+public class Unbelievable {
+    private static Integer i;
+
+    public static void main(String[] args) {
+
+        if (i == 41) {
+            System.out.println("unbelievable");
+        }
+    }
+}
